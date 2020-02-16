8f76a98baca1972a3f81746b444b57d83601b547
Phil Johnson <phytoporg@gmail.com>
2020-02-15 20:44:44 -0800

    RenderDocCompanion, initial revision.

---
+++ RenderDocCompanion/pch.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define NOMINMAX
+
+// System includes
+#include <windows.h>
+
+// DirectX includes
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+// stl
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+// Link library dependencies
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "winmm.lib")
+
+// Safely release a COM object.
+template<typename T>
+inline void SafeRelease(T& ptr)
+{
+    if (ptr != NULL)
+    {
+        ptr->Release();
+        ptr = NULL;
+    }
+}
