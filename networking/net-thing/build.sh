d4ddba966a2773c6cadbaf92311f873564c6a1e0
Phil <phytopoorg@gmail.com>
2025-01-26 17:22:02 -0800

    rearrange net-thing directory structure a bit

--- networking/net-thing/build.sh
+++ networking/net-thing/build.sh
@@ -5,9 +5,9 @@ mkdir build
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 BUILD_DIR="$SCRIPT_DIR/build"
-SERVER_SOURCE_DIR="$SCRIPT_DIR/server"
-CLIENT_SOURCE_DIR="$SCRIPT_DIR/client"
-NET_SOURCE_DIR="$SCRIPT_DIR/net"
+SRC_DIR="$SCRIPT_DIR/src"
+SERVER_SOURCE_DIR="$SRC_DIR/server"
+CLIENT_SOURCE_DIR="$SRC_DIR/client"
 
-gcc -I$SCRIPT_DIR -o "$BUILD_DIR/server" "$SERVER_SOURCE_DIR/main.c"
-gcc -I$SCRIPT_DIR -o "$BUILD_DIR/client" "$CLIENT_SOURCE_DIR/main.c"
+gcc -I$SRC_DIR -o "$BUILD_DIR/server" "$SERVER_SOURCE_DIR/main.c"
+gcc -I$SRC_DIR -o "$BUILD_DIR/client" "$CLIENT_SOURCE_DIR/main.c"
